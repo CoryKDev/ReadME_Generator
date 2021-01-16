a138d9838bbd11797e1f91cf9e55a7e2801caedc
CoryKDev <corykeelan7@gmail.com>
2021-01-15 18:40:43 -0800

    created development enviroment and called all dependancies as well as questions for the generator

---
+++ assets/markdown.js
@@ -0,0 +1,3 @@
+var fs = require("fs");
+
+// fuinction to gereate markdown for README
---
+++ index.js
@@ -0,0 +1,53 @@
+
+const inquirer = require("inquirer");
+const { type } = require("os");
+const fs = require("fs");
+const markdown = require("./assets/markdown");
+
+// questions
+const questions = [
+    {
+        message: "",
+        name: "",
+        type: "",
+      },
+      {
+        message: "",
+        name: "",
+        type: "",
+      },
+      {
+        message: "",
+        name: "",
+        type: "",
+      },
+      {
+        message: "",
+        name: "",
+        type: "",
+      },
+      {
+        message: "",
+        name: "",
+        type: "",
+      },
+      {
+        message: "",
+        name: "",
+        type: "",
+      },
+      {
+        message: "",
+        name: "",
+        type: "",
+      },
+      {
+        message: "",
+        name: "",
+        type: "",
+      },
+
+];
+    
+
+// function to create readme file
